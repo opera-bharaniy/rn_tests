c425e9381532dbaf8fbf57a1ac77fa105ee644e1
yebh17 <sunnyrebels0@gmail.com>
2020-07-26 11:00:23 +0200

    Add android to detox

--- detox_tests.sh
+++ detox_tests.sh
@@ -3,4 +3,4 @@ echo "------------Build the detox tests---------------"
 detox build -c ios.sim.debug && detox build -c android.emu.debug
 
 echo "------------Run the detox tests---------------"
-detox test -c ios.sim.debug detox run -c android.emu.debug
+detox test -c ios.sim.debug && detox test -c android.emu.debug
